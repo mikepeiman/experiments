602d373b8ac2e1181bf6a76800c30fb4e3b4ff8f
Mike Peiman <mikepeiman@gmail.com>
2019-06-05 14:30:26 -0700

    Completed basic functions to convert string to list and create/add those nodes to document

--- src/views/TextEffects.vue
+++ src/views/TextEffects.vue
@@ -3,6 +3,9 @@
 <h1>CSS Text Effects</h1>
 <p id="textToTransform">Malakaya</p>
 <button @click="gettext">Get Text</button>
+<button @click="convertLettersToList">String to array</button>
+<button @click="convertArrayToListItems">Array to list items</button>
+<button @click="setNewHTML">Set new HTML</button>
 </div>
 </template>
 
@@ -15,14 +18,36 @@ export default {
   data() {
     return {
       oldInnerText: '',
+      textArray: [],
       currentLetter: '',
-      newHTML: ''
+      lettersAsListItems: [],
+      newHTML: '',
+      newList: ''
     };
   }, 
   methods: {
     gettext() {
       this.oldInnerText = document.querySelector('#textToTransform').textContent
       console.log(`text content: ${this.oldInnerText}`)
+    },
+    convertLettersToList() {
+      this.textArray = this.oldInnerText.split('')
+      console.log(`array from string: ${this.textArray}`)
+    },
+    convertArrayToListItems() {
+      this.lettersAsListItems = this.textArray.map(x => `<li>${x}</li>`)
+      console.log(`new HTML: ${this.lettersAsListItems}`)
+    },
+    setNewHTML() {
+      let parent = document.querySelector('#textToTransform')
+      let ul = document.createElement('ul')
+      ul.setAttribute('class', 'animated-text')
+      
+      this.lettersAsListItems.forEach(li => {
+        ul.innerHTML += li
+        // console.log(`li: ${li}`)
+      })
+      parent.appendChild(ul)
     }
   }
 };
