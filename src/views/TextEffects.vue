9a8447412b35631f01d6811a4085011687ebc7e6
Mike Peiman <mikepeiman@gmail.com>
2019-06-05 14:12:55 -0700

    Begun adding functionality to take text string from element to manipulate

--- src/views/TextEffects.vue
+++ src/views/TextEffects.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="about">
 <h1>CSS Text Effects</h1>
+<p id="textToTransform">Malakaya</p>
+<button @click="gettext">Get Text</button>
 </div>
 </template>
 
@@ -12,8 +14,17 @@ export default {
 
   data() {
     return {
+      oldInnerText: '',
+      currentLetter: '',
+      newHTML: ''
     };
   }, 
+  methods: {
+    gettext() {
+      this.oldInnerText = document.querySelector('#textToTransform').textContent
+      console.log(`text content: ${this.oldInnerText}`)
+    }
+  }
 };
 </script>
 
