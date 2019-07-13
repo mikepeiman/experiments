191911c8b12358d5cc88248f362fdd403bb91fe8
Mike Peiman <mikepeiman@gmail.com>
2019-07-13 11:57:01 -0700

    Added a simple method to get airtable data; exploring different implementations

--- src/scripts/APIService.js
+++ src/scripts/APIService.js
@@ -1,7 +1,20 @@
 import axios from 'axios'
+import Airtable from 'airtable'
+
 const API_URL = process.env.VUE_APP_AIRTABLE_URI
+const API_KEY = process.env.VUE_APP_AIRTABLE_API_KEY
+const BASE = process.env.VUE_APP_AIRTABLE_BASE
+let airtable = new Airtable({ apiKey: API_KEY, endpointUrl: API_URL })
 export class APIService {
   constructor() {
-    get
+
+  }
+  getItems() {
+    const url = `${API_URL}`
+    return axios.get(url).then(response => response.data)
+  }
+  getItem(id) {
+    const url = `${API_URL}/${id}`
+    return axios.get(url).then(response => response.data)
   }
 }
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,29 +1,60 @@
 <template>
-<div class="about">
-<h1 class="title clear">Blog!</h1>
-<h2 class="subtitle clear">Subtitle</h2>
-<p class="article clear">Some text</p>
-</div>
+  <div class="about">
+    <h1 class="title clear">Blog!</h1>
+    <h2 class="subtitle clear">Subtitle</h2>
+    <p class="article clear">Some text</p>
+    <div v-for="record in records">
+      {{ record }}
+    </div>
+  </div>
 </template>
 
 <script>
 import Color from "color";
+import axios from "axios";
+import Airtable from "airtable";
 
 export default {
   name: "about",
 
   data() {
     return {
+      records: [],
+      API_URL: process.env.VUE_APP_AIRTABLE_URI,
+      API_KEY: process.env.VUE_APP_AIRTABLE_API_KEY,
+      BASE: 'appP3Ar7WtMKMd6Hu/',
+      TABLE: 'Test%20Table/'
     };
-  }, 
+  },
+  created() {
+    // let airtable = new Airtable({ apiKey: this.API_KEY, endpointUrl: this.API_URL });
+    this.getData()
+  },
+  methods: {
+    getData() {
+      // const API_URL = process.env.VUE_APP_AIRTABLE_URI;
+      // const API_KEY = process.env.VUE_APP_AIRTABLE_API_KEY;
+      // const BASE = process.env.VUE_APP_AIRTABLE_BASE;
+      axios({
+        url: this.API_URL + this.BASE + this.TABLE,
+        headers: {
+          Authorization: `Bearer ${this.API_KEY}`
+        }
+      }).then((res => {
+        this.records = res.data.records;
+      }));
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-
 .about {
   display: grid;
-  grid-template-rows: [title] minmax(2em, 1fr) [subtitle] minmax(2em, 1fr) [article] minmax(2em, auto);
+  grid-template-rows: [title] minmax(2em, 1fr) [subtitle] minmax(2em, 1fr) [article] minmax(
+      2em,
+      auto
+    );
 }
 
 .clear {
@@ -34,17 +65,16 @@ export default {
 
 .title {
   grid-row: title;
-  background: rgba(0,0,255,0.5);
+  background: rgba(0, 0, 255, 0.5);
 }
 .subtitle {
   grid-row: subtitle;
-  background: rgba(0,255,0,0.5);
+  background: rgba(0, 255, 0, 0.5);
 }
 .article {
   grid-row: article;
-  background: rgba(255,0,0,0.5);
+  background: rgba(255, 0, 0, 0.5);
   min-height: 100px;
   max-height: 100%;
 }
-
 </style>  
