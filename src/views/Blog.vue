58050d81c748b61be53c5d62c1fd4ca7bbd54c92
Mike Peiman <mikepeiman@gmail.com>
2019-07-13 19:28:59 -0700

    Have implement basic get and post data with axios to and from Airtable. Implemented basic form to add items, button to refresh, and basic flex and grid styling

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="about">
+  <div class="blog">
     <h1 class="title clear">Blog!</h1>
     <h2 class="subtitle clear">Subtitle</h2>
-    <p class="article clear">Some text</p>
-    <div v-for="record in records">
-      {{ record }}
+    <div class="article clear">
+      <div class="products-wrapper">
+        <div class="product-details" v-for="record in records">
+          <div class="single-product-detail">{{ record.fields.make }}</div>
+          <div class="single-product-detail">{{ record.fields.model }}</div>
+          <div class="single-product-detail">{{ record.fields.version}}</div>
+        </div>
+      </div>
     </div>
+    <form class="product-form">
+      <label for="productName">
+        Product name
+        <input type="text" name="productName" id="productName" />
+      </label>
+      <label for="productModel">
+        Product model
+        <input type="text" name="productModel" id="productModel" />
+      </label>
+      <label for="productVersion">
+        Product version
+        <input type="text" name="productVersion" id="productVersion" />
+      </label>
+      <button @click.prevent="postData()">Add New Record</button>
+      <button @click.prevent="getData()">Refresh Records View</button>
+    </form>
   </div>
 </template>
 
@@ -22,13 +43,13 @@ export default {
       records: [],
       API_URL: process.env.VUE_APP_AIRTABLE_URI,
       API_KEY: process.env.VUE_APP_AIRTABLE_API_KEY,
-      BASE: 'appP3Ar7WtMKMd6Hu/',
-      TABLE: 'Test%20Table/'
+      BASE: "appP3Ar7WtMKMd6Hu/",
+      TABLE: "Test%20Table/"
     };
   },
   created() {
     // let airtable = new Airtable({ apiKey: this.API_KEY, endpointUrl: this.API_URL });
-    this.getData()
+    this.getData();
   },
   methods: {
     getData() {
@@ -36,20 +57,79 @@ export default {
       // const API_KEY = process.env.VUE_APP_AIRTABLE_API_KEY;
       // const BASE = process.env.VUE_APP_AIRTABLE_BASE;
       axios({
+        method: "get",
         url: this.API_URL + this.BASE + this.TABLE,
         headers: {
           Authorization: `Bearer ${this.API_KEY}`
         }
-      }).then((res => {
+      }).then(res => {
         this.records = res.data.records;
-      }));
+      });
+    },
+    postData() {
+      let productName = document.querySelector("#productName").value;
+      let productVersion = document.querySelector("#productVersion").value;
+      let productModel = document.querySelector("#productModel").value;
+      console.log(`postData function called with ${productName}`);
+      console.log(`postData function called with ${productModel}`);
+      console.log(`postData function called with ${productVersion}`);
+      axios({
+        method: "post",
+        url: this.API_URL + this.BASE + this.TABLE,
+        headers: {
+          Authorization: `Bearer ${this.API_KEY}`
+        },
+        data: {
+          fields: {
+            make: productName,
+            model: productModel,
+            version: productVersion
+          }
+        }
+      }).then(res => {
+        this.records = res.data.records;
+      });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.about {
+label {
+  background: rgba(0, 0, 0, 0.25);
+  padding: 0.5rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  & input {
+    margin-left: 1rem;
+  }
+}
+
+.product-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.products-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.product-details {
+  display: grid;
+  grid-template-columns: [make] 1fr [model] 1fr [version] 1fr;
+  width: 100%;
+  justify-items: center;
+}
+
+.single-product-detail {
+  display: flex;
+  justify-content: flex-start;
+}
+.blog {
   display: grid;
   grid-template-rows: [title] minmax(2em, 1fr) [subtitle] minmax(2em, 1fr) [article] minmax(
       2em,
